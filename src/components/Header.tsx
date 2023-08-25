4de532a50a373eeea96e3b5212b00638f370c853
Jia Wei Hong <jiaweihong1@hotmail.com>
2023-08-25 16:23:14 +0800

    add mobile responsiveness to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,86 @@
 import Link from 'next/link'
-import React from 'react'
-import { faRulerVertical } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect, useState } from 'react'
+import { faRulerVertical, faBars, faX } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Header() {
-  return (
-    <ul className="flex h-20 items-center justify-between bg-[#CD2444] px-16 text-lg text-[#FFFFFF]">
-      <li className="text-2xl">
-        <FontAwesomeIcon
-          className="pr-2"
-          icon={faRulerVertical}
-        />
-        <Link
-          href="/"
-          className="font-bebasNeue"
-        >
-          VertCheck
-        </Link>
-      </li>
+  const [isMobileNavbarOpened, setIsMobileNavbarOpened] = useState(false)
+  function toggleMobileNavbar() {
+    setIsMobileNavbarOpened(!isMobileNavbarOpened)
+  }
 
-      <div className="flex items-center justify-center">
-        <li className="">
-          <Link href="/calculate-vertical">Calculate My Vertical</Link>
+  return (
+    <header className="bg-[#CD2444] px-16 text-lg text-[#FFFFFF]">
+      <ul className="flex h-20 items-center justify-between ">
+        <li className="text-2xl">
+          <FontAwesomeIcon
+            className="pr-2"
+            icon={faRulerVertical}
+          />
+          <Link
+            href="/"
+            className="font-bebasNeue"
+          >
+            VertCheck
+          </Link>
         </li>
-        <li className="pl-8 ">
-          <Link href="/faq">FAQ</Link>
-        </li>
-      </div>
-    </ul>
+
+        <div>
+          <button
+            className="block sm:hidden"
+            onClick={toggleMobileNavbar}
+          >
+            {isMobileNavbarOpened && (
+              <FontAwesomeIcon
+                className="text-xl"
+                icon={faX}
+              />
+            )}
+            {!isMobileNavbarOpened && (
+              <FontAwesomeIcon
+                className="text-xl"
+                icon={faBars}
+              />
+            )}
+          </button>
+
+          <div className="hidden items-center justify-center sm:flex">
+            <li>
+              <Link href="/calculate-vertical">Calculate My Vertical</Link>
+            </li>
+            <li className="pl-8 ">
+              <Link href="/faq">FAQ</Link>
+            </li>
+          </div>
+        </div>
+      </ul>
+
+      {isMobileNavbarOpened && (
+        <div className="bg-[#CD2444] sm:hidden">
+          <ul className="flex flex-col items-end">
+            <li className="pb-4">
+              <Link
+                href="/calculate-vertical"
+                onClick={() => {
+                  toggleMobileNavbar()
+                }}
+              >
+                Calculate My Vertical
+              </Link>
+            </li>
+            <li className="pb-4 ">
+              <Link
+                href="/faq"
+                onClick={() => {
+                  toggleMobileNavbar()
+                }}
+              >
+                FAQ
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
   )
 }
