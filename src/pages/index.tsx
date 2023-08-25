b8686f287e0b5dbd301b9d7d80cd50304ed44f44
Jia Wei Hong <jiaweihong1@hotmail.com>
2023-08-25 12:09:02 +0800

    add responsiveness to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Home() {
         <img
           src="/images/hero-bg.jpg"
           alt="Image"
-          className="brightness-[35%]"
+          className="min-h-[600px] brightness-[35%]"
         />
 
-        <div className="absolute text-center">
+        <div className="absolute px-4 text-center">
           <h1 className="font-bebasNeue text-8xl text-[#CD2444]">
             Find out how high you jump
           </h1>
@@ -37,8 +37,8 @@ export default function Home() {
           3 Simple Steps
         </h2>
 
-        <div className="flex w-[75%] flex-wrap justify-around">
-          <div className="w-[250px] rounded-2xl border border-gray-300 px-16 py-10 text-center shadow">
+        <div className="flex min-h-[800px] w-[80%] flex-col flex-wrap items-center justify-between text-center sm:min-h-[auto] sm:max-w-[1200px] sm:flex-row">
+          <div className="w-[250px] rounded-2xl border border-gray-300 px-10 py-10 shadow">
             <FontAwesomeIcon
               icon={faUpload}
               className="pb-4 text-3xl text-[#CD2444]"
@@ -49,7 +49,7 @@ export default function Home() {
             <p className="text-gray-700">Upload your video onto the website</p>
           </div>
 
-          <div className="w-[250px] rounded-2xl border border-gray-300 px-16 py-10 text-center shadow">
+          <div className="w-[250px] rounded-2xl border border-gray-300 px-10 py-10 shadow">
             <FontAwesomeIcon
               icon={faMarker}
               className="pb-4 text-3xl text-[#CD2444]"
@@ -59,7 +59,7 @@ export default function Home() {
             <p className="text-gray-700">Mark the start and end of the jump</p>
           </div>
 
-          <div className="w-[250px] rounded-2xl border border-gray-300 px-16 py-10 text-center shadow">
+          <div className="w-[250px] rounded-2xl border border-gray-300 px-10 py-10 shadow">
             <FontAwesomeIcon
               icon={faCalculator}
               className="pb-4 text-3xl text-[#CD2444]"
