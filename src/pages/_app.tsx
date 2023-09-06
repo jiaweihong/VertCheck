e0bcfd275b36be55ab51062c24f8e4d327ea88b7
Jia Wei Hong <jiaweihong1@hotmail.com>
2023-09-06 17:56:08 +0800

    add a firebase app to allow for analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import Layout from '../components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { app } from '../../firebase'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const a = app
   return (
     <Layout>
       <Component {...pageProps} />
